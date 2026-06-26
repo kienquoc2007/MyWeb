5880ea14a510467e1dc5784043bb13f129726b43
kienquoc2007 <quocpham.150807@gmail.com>
2026-06-26 09:48:21 +0700

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const siteName = "MyWeb";
+let topic = "Chủ đề website cá nhân của em";
+let imageCount = 5;
+let isReady = true;
+
+console.log("Website:", siteName);
+console.log("Chủ đề:", topic);
+console.log("Số ảnh:", imageCount);
+console.log("Đã sẵn sàng dùng JS?", isReady);
+console.log("Kiểu dữ liệu topic:", typeof topic);
